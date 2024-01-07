40e7a0d08cc338060c457db00012e98fe381f9f2
Tayfun Açıcı <tyfnacici@gmail.com>
2024-01-07 19:51:49 +0300

    add crud

--- app/dashboard/dersler/page.jsx
+++ app/dashboard/dersler/page.jsx
@@ -1,10 +1,77 @@
 "use client"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Modal from "@/app/components/modal"
-import { useState } from "react"
+import { notFound } from "next/navigation"
 
-export default function page() {
+async function getCourses() {
+  const res = await fetch("http://localhost:8000/api/course", {
+    cache: "no-store",
+  })
+  if (!res.ok) return notFound()
+  return res.json()
+}
+
+async function createCourse() {
+  const res = await fetch("http://localhost:8000/api/course", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  if (!res.ok) return notFound()
+  return res.json()
+}
+
+async function updateCourse(id, data) {
+  const res = await fetch(`http://localhost:8000/api/course/${id}`, {
+    method: "POST",
+    body: data,
+  })
+  if (!res.ok) return notFound()
+}
+
+async function deleteCourse(id, data) {
+  const res = await fetch(`http://localhost:8000/api/course/${id}`, {
+    method: "DELETE",
+  })
+  if (!res.ok) return notFound()
+}
+
+async function getCourseByID(id) {
+  const res = await fetch(`http://localhost:8000/api/course/${id}`, {
+    cache: "no-store",
+  })
+  if (!res.ok) return notFound()
+  return res.json()
+}
+
+export default function Page() {
   const [showModal, setShowModal] = useState(false)
+  const [courses, setCourses] = useState([])
+  const [selectedCourse, setSelectedCourse] = useState(null)
+  const [modalValue, setModalValue] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const coursesData = await getCourses()
+        setCourses(coursesData.data)
+      } catch (error) {
+        console.error("Error fetching course data", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  async function openModal(course) {
+    const res = await getCourseByID(course?._id)
+    setModalValue(JSON.stringify(res.data, null, "\t"))
+    setSelectedCourse(res.data) // Set selectedCourse for other purposes if needed
+    setShowModal(true)
+  }
+
   return (
     <>
       <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
@@ -12,15 +79,24 @@ export default function page() {
           <h3 className="text-xl font-semibold">Ders Oluştur</h3>
           <form action="">
             <textarea
-              placeholder="json datası olacak burada"
+              value={modalValue}
+              onChange={(e) => setModalValue(e.target.value)}
               className="w-full my-5 border-2 border-gray-300 rounded h-64"
               type="text"
             />
             <button
               className="place-self-start bg-blue-500 text-white px-4 py-2 rounded"
-              type="submit"
+              type="button"
+              onClick={() => updateCourse(selectedCourse?._id, modalValue)}
             >
               Oluştur
+            </button>
+            <button
+              className="place-self-end bg-red-500 text-white px-4 py-2 rounded"
+              type="button"
+              onClick={() => deleteCourse(selectedCourse?._id)}
+            >
+              Delete
             </button>
           </form>
         </div>
@@ -56,110 +132,37 @@ export default function page() {
             </tr>
           </thead>
           <tbody>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+            {courses.map((course) => (
+              <tr
+                className="odd:bg-white even:bg-gray-50 border-b"
+                key={course?._id}
               >
-                Bilişim Sistemleri Mühendisliğine Giriş
-              </th>
-              <td className="px-6 py-4">1041</td>
-              <td className="px-6 py-4">Serdar Solak</td>
-              <td className="px-6 py-4">120</td>
-              <td className="px-6 py-4">
-                <button
-                  onClick={() => setShowModal(true)}
-                  className="font-medium text-blue-600  hover:underline"
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
+                  {course?.ders_ismi}
+                </th>
+                <td className="px-6 py-4">{course?.sinif}</td>
+                <td className="px-6 py-4">{course?.egitmen_id}</td>
+                <td className="px-6 py-4">{course?.ogrenci_sayisi}</td>
+                <td className="px-6 py-4">
+                  <button
+                    onClick={() => openModal(course)}
+                    className="font-medium text-blue-600  hover:underline"
                   >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                Bilişim Sistemleri Mühendisliğine Giriş
-              </th>
-              <td className="px-6 py-4">1041</td>
-              <td className="px-6 py-4">Serdar Solak</td>
-              <td className="px-6 py-4">120</td>
-              <td className="px-6 py-4">
-                <button
-                  onClick={() => setShowModal(true)}
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                Bilişim Sistemleri Mühendisliğine Giriş
-              </th>
-              <td className="px-6 py-4">1041</td>
-              <td className="px-6 py-4">Serdar Solak</td>
-              <td className="px-6 py-4">120</td>
-              <td className="px-6 py-4">
-                <button
-                  href="#"
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                Bilişim Sistemleri Mühendisliğine Giriş
-              </th>
-              <td className="px-6 py-4">1041</td>
-              <td className="px-6 py-4">Serdar Solak</td>
-              <td className="px-6 py-4">120</td>
-              <td className="px-6 py-4">
-                <button
-                  onClick={() => setShowModal(true)}
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      width="24"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- app/dashboard/egitmenler/page.jsx
+++ app/dashboard/egitmenler/page.jsx
@@ -1,23 +1,94 @@
 "use client"
-
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Modal from "@/app/components/modal"
+import { notFound } from "next/navigation"
 
-export default function page() {
+async function getInstructors() {
+  const res = await fetch("http://localhost:8000/api/instructor", {
+    cache: "no-store",
+  })
+  if (!res.ok) return notFound()
+  return res.json()
+}
+
+async function createInstructor() {
+  const res = await fetch("http://localhost:8000/api/instructor", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  if (!res.ok) return notFound()
+  return res.json()
+}
+
+async function updateInstructor(id, data) {
+  const res = await fetch(`http://localhost:8000/api/instructor/${id}`, {
+    method: "POST",
+    body: data,
+  })
+  if (!res.ok) return notFound()
+}
+
+async function deleteInstructor(id) {
+  const res = await fetch(`http://localhost:8000/api/instructor/${id}`, {
+    method: "DELETE",
+  })
+  if (!res.ok) return notFound()
+}
+
+async function getInstructorByID(id) {
+  const res = await fetch(`http://localhost:8000/api/instructor/${id}`, {
+    cache: "no-store",
+  })
+  if (!res.ok) return notFound()
+  return res.json()
+}
+
+export default function Page() {
+  const [showModal, setShowModal] = useState(false)
+  const [instructors, setInstructors] = useState([])
+  const [selectedInstructor, setSelectedInstructor] = useState(null)
+  const [modalValue, setModalValue] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const instructorsData = await getInstructors()
+        setInstructors(instructorsData.data)
+      } catch (error) {
+        console.error("Error fetching instructor data", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  async function openModal(instructor) {
+    const res = await getInstructorByID(instructor?._id)
+    setModalValue(JSON.stringify(res.data, null, "\t"))
+    setSelectedInstructor(res.data)
+    setShowModal(true)
+  }
+
   return (
     <>
+      <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
+        {/* Modal Content */}
+      </Modal>
       <div className="overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-400">
           <thead className="text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" className="px-8 py-6">
-                Hoca ismi
+                Hoca İsmi
               </th>
               <th scope="col" className="px-8 py-6">
                 Verdiği Dersler
               </th>
               <th scope="col" className="px-8 py-6">
-                <button>
+                <button onClick={() => setShowModal(true)}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="16"
@@ -31,128 +102,36 @@ export default function page() {
             </tr>
           </thead>
           <tbody>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+            {instructors.map((instructor) => (
+              <tr
+                className="odd:bg-white even:bg-gray-50 border-b"
+                key={instructor?._id}
               >
-                Serder Solak
-              </th>
-              <td className="px-6 py-4">
-                Bilişim Sistemleri Mühendisliğine Giriş
-              </td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600  hover:underline"
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
+                  {instructor?.hoca_ismi}
+                </th>
+                <td className="px-6 py-4">{instructor?.verdigi_dersler}</td>
+                <td className="px-6 py-4">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 hover:underline"
+                    onClick={() => openModal(instructor)}
                   >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                dshjlfsahfashafs
-              </th>
-              <td className="px-6 py-4">lorem</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                asd
-              </th>
-              <td className="px-6 py-4">Sgdanjsfajklfsa</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                dshjlfsahfashafs
-              </th>
-              <td className="px-6 py-4">lorem</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </a>
-              </td>
-            </tr>
-            <tr className="odd:bg-white  even:bg-gray-50  border-b">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-              >
-                dshjlfsahfashafs
-              </th>
-              <td className="px-6 py-4">lorem</td>
-              <td className="px-6 py-4">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600  hover:underline"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-                  </svg>
-                </a>
-              </td>
-            </tr>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      width="24"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                    </svg>
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
